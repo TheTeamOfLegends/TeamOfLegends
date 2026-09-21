c3f8cbdf98c2f8e561ac297f67e5cb47e362eb0e
Alexey Soloviev <alexey.soloviev.p@gmail.com>
2026-09-21 22:51:00 +0300

    Add migrations to create themes, theme_users and user_themes tables

---
+++ packages/server/src/migrations/20260921220002-create-theme-users-table.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('theme_users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      userId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('theme_users')
+  }
+}
